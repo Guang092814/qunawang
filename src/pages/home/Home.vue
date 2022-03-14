8ced4753b2a80a726b3ac53cc9bd3843f15b5eee
zhang <2055652353@qq.com>
2022-03-14 10:57:47 +0800

    头部3月14号上午十一点

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header.vue'
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names
+        name:"Home",
+        components:{
+            HomeHeader
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xeb6d;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader",
+};
+</script>
+
+<style lang="stylus" scoped>
+.header {
+  display: flex;
+  line-height: 43px;
+  background: #00bcd4;
+  color: #fff;
+
+  .header-left {
+    width: 32px;
+    float: left;
+    .back-icon{
+      text-align: center
+      font-size: 20px
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    height: 32px;
+    line-height: 32px;
+    margin-top: 6px;
+    margin-left: 10px;
+    padding-left: 10px
+    background: #fff;
+    border-radius: 5px;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 62px;
+    float: right;
+    text-align: center;
+    .arrow-icon{
+      margin-left: -2px 
+      font-size: 12px 
+    }
+  }
+}
+</style>
