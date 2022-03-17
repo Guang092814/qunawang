7f50bd12bc7ada7d60b8edcdf7184da91560c66f
zhang <2055652353@qq.com>
2022-03-17 15:17:03 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
-        <CityHeader/>
+        <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
+import CitySearch from './components/Search.vue'
 import CityHeader from './components/Header.vue'
     export default {
         // eslint-disable-next-line vue/multi-word-component-names
         name:"City",
         components:{
             CityHeader,
+            CitySearch,
         }
     }
 </script>
