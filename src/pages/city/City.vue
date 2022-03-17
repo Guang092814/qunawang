b38b802be9c97e0eb42ef51f725ac776e50e7ce2
zhang <2055652353@qq.com>
2022-03-17 16:26:12 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,24 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
 import CitySearch from './components/Search.vue'
 import CityHeader from './components/Header.vue'
+import CityList from './components/List.vue'
+import CityAlphabet from './components/Alphabet.vue' 
     export default {
         // eslint-disable-next-line vue/multi-word-component-names
         name:"City",
         components:{
             CityHeader,
             CitySearch,
+            CityList,
+            CityAlphabet
         }
     }
 </script>
