5639c3612232fbc0cc6d84cbf139953aa7ab7c33
zhang <2055652353@qq.com>
2022-03-22 10:01:21 +0800

    header-animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,26 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
+import DetailHeader from './components/Header.vue'
     export default {
         // eslint-disable-next-line vue/multi-word-component-names
         name:"Detail",
         components:{
             DetailBanner,
+            DetailHeader,
         }
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="stylus" scoped>
+.content{
+    height: 50rem;
+}
 </style>
