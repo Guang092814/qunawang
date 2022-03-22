2122a6ab68f1ceda98564cf62bf2fff9e679852e
zhang <2055652353@qq.com>
2022-03-22 13:40:35 +0800

    add detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,26 +1,58 @@
 <template>
-    <div>
-        <detail-banner></detail-banner>
-        <detail-header></detail-header>
-        <div class="content"></div>
+  <div>
+    <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
     </div>
+  </div>
 </template>
 
 <script>
-import DetailBanner from './components/Banner.vue'
-import DetailHeader from './components/Header.vue'
-    export default {
-        // eslint-disable-next-line vue/multi-word-component-names
-        name:"Detail",
-        components:{
-            DetailBanner,
-            DetailHeader,
-        }
-    }
-</script>
+import DetailBanner from "./components/Banner.vue";
+import DetailHeader from "./components/Header.vue";
+import DetailList from "./components/List.vue";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Detail",
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList,
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children:[{
+              title:"成人三",
+              children:[{
+                  title:"成人三-墨家常人能"
+              }]
+          },
+          {
+              title:"成人无"
+          }]
+          
+        },
+        {
+          title: "学生票",
+        },
+        {
+          title: "儿童票",
+        },
+        {
+          title: "特惠票",
+        },
+      ],
+    };
+  },
+};
+</script>D
 
 <style lang="stylus" scoped>
-.content{
-    height: 50rem;
+.content {
+  height: 50rem;
 }
 </style>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,18 +1,9 @@
 <template>
   <div>
-    <router-link 
-    tag="div" 
-    to="/" 
-    class="header-abs"
-    v-show="showAbs"
-    >
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
       <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
-    <div 
-    class="header-fixed" 
-    v-show="!showAbs"
-    :style="opacityStyle"
-    >
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
       <router-link to="/">
         <div class="iconfont header-fixed-back">&#xe624;</div>
       </router-link>
@@ -24,38 +15,39 @@
 <script>
 export default {
   name: "DetailHeader",
-  data(){
-      return{
-          showAbs: true,
-          opacityStyle:{
-              opacity: 0
-          }
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0,
+      },
+    };
+  },
+  methods: {
+    handleScorll() {
+      const top = document.documentElement.scrollTop;
+      if (top > 60) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityStyle = { opacity };
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
       }
+    },
   },
-  methods:{
-      handleScorll(){
-          const top =document.documentElement.scrollTop
-         if(top > 60){
-             let opacity =top/140
-             opacity = opacity > 1 ? 1: opacity
-             this.opacityStyle = { opacity }
-             this.showAbs = false;
-         }else{
-             this.showAbs =true
-         }
-      }
+  activated() {
+    window.addEventListener("scroll", this.handleScorll);
   },
-  activated(){
-      window.addEventListener('scroll', this.handleScorll)
+  deactivated() {
+    window.removeEventListener("scroll", this.handleScorll);
   },
-  deactivated(){
-      window.removeEventListener('scroll', this.handleScorll)
-  }
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '@/assets/style/varibles.styl';
+
 .header-abs {
   position: absolute;
   left: 0.2rem;
@@ -74,12 +66,13 @@ export default {
 }
 
 .header-fixed {
+  z-index: 2;
   position: fixed;
-  top:0;
+  top: 0;
   left: 0;
-  right 0;
+  right: 0;
   height: $headerHeight;
-  line-height: $headerHeight;
+  line-height: $headerHeight; 
   text-align: center;
   color: #fff;
   background: $bgColor;
